a3dfba3d06c69ceb2c6284499e4bf3b13920ef14
Field User <field@ccom.unh.edu>
2021-10-29 14:23:50 -0400

    removes msg extract function and adds base pkg generation

--- msgext/msgext.py
+++ msgext/msgext.py
@@ -3,35 +3,34 @@
 import rosbag
 import sys
 import argparse
+import os
 
 def main():
 
-    original_stdout = sys.stdout
-    my_parser = argparse.ArgumentParser(
-                    prog='myls',
-                    usage='%(prog)s [options] path',
-                    description='List the message types in a bag')
-
-    my_parser.add_argument(
-                    'Path to bag',
-                    metavar='path',
-                    type=str,
-                    help='The bag file to proccess')
-
-    args = my_parser.parse_args()
-    input_path = args.Path
-
+    base_path = 'pkg'
+    os.mkdir(base_path) 
     types = {}
 
     for bagname in sys.argv[1:]:
         bag = rosbag.Bag(bagname)
         for topic, msg, t in bag.read_messages():
             types[msg._type] = msg._full_text 
-
     with open(bagname + '_msg_types.txt', 'w') as f:
-        sys.stdout = f
+        # sys.stdout = f
         for t in types:
-            print ("Message type:", t)
-            print ("Message text:")
-            print (types[t])
-        sys.stdout = original_stdout
+            direct = base_path
+            direct_arr = t.split('/')
+            #print (direct_arr)
+            for i in direct_arr:
+                direct = (direct + '/' + i)
+                try:
+                    os.mkdir(direct)
+                except FileExistsError:
+                    print()
+                
+
+
+            # print ("Message type:", t)
+            # print ("Message text:")
+            # print (types[t])
+            # print ()
